a57a1daea591ed4774a5a902bdb122b8815088d8
Sthefany Yasmin Diehl <sthefany.diehl@gmail.com>
2024-06-19 21:46:18 -0300

    primeiro exercicio em javascript

---
+++ e.js
@@ -0,0 +1,34 @@
+let name = "joão"
+console.log(nome)
+
+let a=5 , b=10, soma=a+b
+console.log(soma) 
+
+let nome = 'josefa', ultimoNome = 'silva', junta= nome+ultimoNome
+console.log(junta)
+
+let pessoa = (nome= 'josefa' ,idade = 15 ,endereço =  116,telefone = 47997929210)
+console.log(pessoa)
+ 
+let frutas =['maça', 'banana', 'laranja']
+frutas.push('uva')
+console.log(frutas)
+
+let numeros=[1,2,3,4,5]
+console.log(numeros [numeros.lenght -1])
+
+let cores=['vermelho', 'verde', 'azul']
+
+for(let i=0;i<cores.leght;i++)
+
+    console.log(cores[i])
+
+    let idade=26
+    if (idade<12)
+        console.log('criança')
+    else if (idade<18)
+        console.log (adolescente)
+    else if (idade<60)
+        console.log (adulto)
+    else 
+    console.log (idoso)
