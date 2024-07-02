816e22ac6092f5a05f9453e38c3cd483a58316f4
Sthefany Yasmin Diehl <sthefany.diehl@gmail.com>
2024-07-01 21:43:21 -0300

    criada a selecao de exercicios

--- e.js
+++ e.js
@@ -1,5 +1,5 @@
 let name = "joão"
-console.log(nome)
+console.log(name)
 
 let a=5 , b=10, soma=a+b
 console.log(soma) 
@@ -7,28 +7,93 @@ console.log(soma)
 let nome = 'josefa', ultimoNome = 'silva', junta= nome+ultimoNome
 console.log(junta)
 
-let pessoa = (nome= 'josefa' ,idade = 15 ,endereço =  116,telefone = 47997929210)
+let pessoa={nome: 'josefa' ,idade: 15, endereço:  116, telefone: 47997929210}
 console.log(pessoa)
  
 let frutas =['maça', 'banana', 'laranja']
 frutas.push('uva')
 console.log(frutas)
 
-let numeros=[1,2,3,4,5]
+let numeros= [1,2,3,4,5]
 console.log(numeros [numeros.lenght -1])
 
 let cores=['vermelho', 'verde', 'azul']
 
 for(let i=0;i<cores.leght;i++)
 
-    console.log(cores[i])
+ console.log(cores[i])
 
-    let idade=26
-    if (idade<12)
-        console.log('criança')
-    else if (idade<18)
-        console.log (adolescente)
-    else if (idade<60)
-        console.log (adulto)
-    else 
-    console.log (idoso)
+let idade = 20;
+ if (idade>=18)
+    console.log('maior de idade')
+else
+console.log('menor de idade')
+
+idade=26
+ if (idade<12)
+   console.log('criança')
+  else if (idade<18) 
+  console.log ('adolescente')
+ else if (idade<60)
+  console.log ('adulto')
+ else 
+ console.log ('idoso')
+
+let nota = 6.5; // Exemplo: atribuindo a nota 6.5
+if (nota >= 7){
+    console.log('aprovado');
+}else if (nota > 5){
+    console.log('recuperação');
+}else{
+    console.log('reprovado');
+}
+
+let diaDaSemana = 3; // Exemplo: atribuindo o valor 3, que representa  quarta-feira
+switch (diaDaSemana) {
+    case 1:
+    console.log('Domingo');
+    break;
+    case 2:
+    console.log('Segunda-feira');
+    break;
+    case 3:
+    console.log('Terça-feira')
+    break;
+    case 4:
+    console.log('Quarta-feira')
+    break;
+    case 5:
+    console.log('Quinta-feira')
+    break;
+    case 6:
+    console.log('Sexta-feira')    
+    break;
+    case 7:
+    console.log('Sábado')
+    default:
+    console.log('valor inválido')
+}
+
+for (let i = 1; i <= 10; i++)
+ console.log(i)
+
+let contador = 1 
+while (contador <= 10){
+console.log (contador);
+contador++;
+}
+ let numero = 0
+ while (numero<=3){
+    console.log (numero)
+    numero++
+ }
+
+ let valores = [10, 20, 30, 40, 50];
+ soma = [0];
+ for (let i =0; i < valores.length; i++){
+soma+=valores[i];
+ }
+ console.log ('A soma dos elementos do vetor é:' soma);{   
+ }
+ 
+ vetor = [50,39,58,48]
